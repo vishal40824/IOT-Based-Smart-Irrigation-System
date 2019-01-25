6024ad0610ac1ee70b428f021c71e693ad87cfae
Vishal Joshi <41179060+vishal40824@users.noreply.github.com>
2019-01-25 18:29:15 +0530

    Update soilData.js

--- public/soilData.js
+++ public/soilData.js
@@ -1,9 +1,8 @@
 var socket = io.connect("http://192.168.43.122:3300");
 var output = document.getElementById("soilInp");
+
 // Listen for events
-// output.innerHTML = "<p>asdfa</p>";
 socket.on('soilValue', function (data) {
    console.log(data);
    output.innerHTML = "<p>Your client No. is: " + data.clientNo + "</p><p>The Current moisture level is: <strong>" + data.soilData + "%</strong></p>"
-   // output.innerHTML = '<p>The Current moisture level is: <strong>' + data.soilData + '%</strong></p><p>Your client No. is: ' + data.clientNo + '</p>';
 });
